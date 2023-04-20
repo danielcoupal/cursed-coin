4a957f8584551a0c352568638a6b125c24e742da
Daniel Coupal <dacoupal@gmail.com>
2023-04-19 22:36:32 -0400

    Only display camera-facing side of coin

--- src/main.c
+++ src/main.c
@@ -10,8 +10,8 @@
 typedef struct g_coin
 {
     double  thick;
-    double  r;
-    double  r_empt;
+    double  r_out;
+    double  r_inner;
     double  z_rad;
 } coin_t;
 
@@ -43,6 +43,7 @@ int main()
 
         stop2 = clock();
         g_coin.z_rad += g_spin * ((double)(stop2 - stop1) / CLOCKS_PER_SEC);
+        g_coin.z_rad = fmod(g_coin.z_rad, M_PI * 2);
         draw_g_coin();
 
         int term_y = tb_height() - 3;
@@ -60,10 +61,10 @@ int main()
 
 void define_g_coin()
 {
-    g_coin.r      = 20.0;
-    g_coin.r_empt = 17.5;
-    g_coin.thick  = 1.0;
-    g_coin.z_rad  = 45;
+    g_coin.r_out      = 20.0;
+    g_coin.r_inner = 15.5;
+    g_coin.thick  = 3.0;
+    g_coin.z_rad  = 0;
 }
 
 void draw_g_coin()
@@ -74,36 +75,61 @@ void draw_g_coin()
         {
             double off_x = (double)(x - (tb_width() / 2));
             double off_z = (double)(z - (tb_height() / 2));
+#if 0
             char c = is_rim_surface(&g_coin, off_x / 2, 0, off_z) ? '#' : ' ';
             tb_set_cell(x, z, c, TB_YELLOW, TB_BLACK);
+#endif
+            if(is_rim_edge(&g_coin, off_x / 2, 0, off_z))
+            {
+                tb_set_cell(x, z, '#', TB_YELLOW, TB_BLACK);
+            }
+            else if(is_rim_side(&g_coin, off_x / 2, 0, off_z))
+            {
+                tb_set_cell(x, z, '#', TB_GREEN, TB_BLACK);
+            }
+            else
+            {
+            }
         }
     }
 }
 
 static bool is_rim_surface(coin_t * p_coin, double x, double y, double z)
 {
+    //return is_rim_side(p_coin, x, y, z);
+    //return is_rim_edge(p_coin, x, y, z);
     return is_rim_side(p_coin, x, y, z) || is_rim_edge(p_coin, x, y, z);
 }
 
 static bool is_rim_edge(coin_t * p_coin, double x, double y, double z)
 {
+    double edge_xdist = sqrt(pow(p_coin->r_out, 2) - pow(z, 2)) *
+                                            cos(p_coin->z_rad);
     double thick_vis = p_coin->thick * fabs(sin(p_coin->z_rad));
-    for(double i = thick_vis; i > 0.0; i -= 0.2)
-    {
-        if(is_rim_side(p_coin, x - i, y, z) 
-        || is_rim_side(p_coin, x + i, y, z))
-        {
-            return true;
-        }
-    }
-    return false;
+
+    x = ((M_PI * 0.0 < p_coin->z_rad) && (p_coin->z_rad < M_PI * 1.0)) ? x : -x;
+
+    bool b_ascend = 
+        ((M_PI * 0.0 < p_coin->z_rad) && (p_coin->z_rad < M_PI * 0.5)) ||
+        ((M_PI * 1.0 < p_coin->z_rad) && (p_coin->z_rad < M_PI * 1.5));
+
+    bool b_x_on_edge = b_ascend ? 
+        ((edge_xdist - thick_vis / 2) < x) && (x < (edge_xdist + thick_vis / 2)) :
+        (x < (edge_xdist + thick_vis / 2)) && ((edge_xdist - thick_vis / 2) < x);
+    return b_x_on_edge;
 }
 
 static bool is_rim_side(coin_t * p_coin, double x, double y, double z)
 {
+    double thick_vis = p_coin->thick * fabs(sin(p_coin->z_rad));
+    bool b_ascend = 
+        ((M_PI * 0.0 < p_coin->z_rad) && (p_coin->z_rad < M_PI * 0.5)) ||
+        ((M_PI * 1.0 < p_coin->z_rad) && (p_coin->z_rad < M_PI * 1.5));
+    b_ascend ? (x += thick_vis / 2) : (x -= thick_vis / 2);
+
     double radius = sqrt((pow(x, 2) / fabs(cos(p_coin->z_rad))) + pow(z, 2));
-    bool b_incl = radius < p_coin->r;
-    bool b_excl = radius < p_coin->r_empt;
+    bool b_incl = radius < p_coin->r_out;
+    bool b_excl = radius < p_coin->r_inner;
     return b_incl && !b_excl;
 }
 
